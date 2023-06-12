b5a850a2b418d29f38a1fa1d2721606266e04ef9
Kostadin Slavov <kostadin.slavov@tixi.bg>
2023-06-12 21:09:19 +0300

    WIP add new template

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,9 +3,11 @@ from django.views import View
 
 
 class HomeView(View):
-    template_name = "home.html"
+    template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return render(request, self.template_name)
+            context = dict(user=request.user)
+
+            return render(request, self.template_name, context=context)
         return render(request, 'login-form.html')
