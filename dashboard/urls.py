7da7d5344dc4a88cf8acd07ddd805beffa5ad2ee
Kostadin Slavov <kostadin.slavov@tixi.bg>
2023-06-26 21:08:34 +0300

    Add the create camera modal

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from dashboard.views import HomeView
+from dashboard.views import HomeView, AddCameraView
 
 urlpatterns = [
     path('', HomeView.as_view()),
-    path('<str:data>/', HomeView.as_view())
+    path('<str:data>/', HomeView.as_view()),
+    path('cameras/add/', AddCameraView.as_view())
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
-
+from django.contrib import messages
 
 class HomeView(View):
     template_name = "index.html"
@@ -13,3 +13,10 @@ class HomeView(View):
 
             return render(request, self.template_name, context=context)
         return render(request, 'login-form.html')
+
+
+class AddCameraView(View):
+    def post(self, request):
+        print(request.POST)
+        messages.success(request, 'Camera added successfully.')
+        return HttpResponseRedirect('/cameras/')
