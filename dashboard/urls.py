29bebd4a9ed2ac85e5aa6691c7861052f636aada
Kostadin Slavov <kostadin.slavov@tixi.bg>
2023-06-26 20:47:19 +0300

    Create the cameras page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from dashboard.views import HomeView
 
 urlpatterns = [
-    path('', HomeView.as_view())
+    path('', HomeView.as_view()),
+    path('<str:data>/', HomeView.as_view())
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,9 +5,11 @@ from django.views import View
 class HomeView(View):
     template_name = "index.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request,data=None, *args, **kwargs):
         if request.user.is_authenticated:
             context = dict(user=request.user)
+            if data == "cameras":
+                self.template_name = "cameras.html"
 
             return render(request, self.template_name, context=context)
         return render(request, 'login-form.html')
