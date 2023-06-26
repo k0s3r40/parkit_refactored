b55e95f7b23d2fc93400e6d0ac27da78d21cdf76
Kostadin Slavov <kostadin.slavov@tixi.bg>
2023-06-26 21:59:27 +0300

    Add create camera view and make it work properly

--- cameras/models.py
+++ cameras/models.py
@@ -1,3 +1,4 @@
+from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
 from django.contrib.gis.db import models
 
@@ -7,13 +8,18 @@ class Camera(models.Model):
         verbose_name = _('Камера')
         verbose_name_plural = _('Камери')
 
+    class Zone(TextChoices):
+        BLUE = 'blue'
+        GREEN = 'green'
+
     uid = models.UUIDField(primary_key=True, unique=True)
+    name = models.CharField(blank=True, null=True, verbose_name=_('име'))
+    address = models.CharField(blank=True, null=True, verbose_name=('адрес'))
     latitude = models.DecimalField(max_digits=9, decimal_places=6, verbose_name=_('ширина'))
     longitude = models.DecimalField(max_digits=9, decimal_places=6, verbose_name=_('дължина'))
-    rotation = models.DecimalField(max_digits=9, decimal_places=6, verbose_name=_('ротация спрямо север'))
     url = models.URLField(blank=False, null=False, verbose_name=_('линк'))
-    parking_mask = models.MultiPolygonField()
-    violator_mask = models.MultiPolygonField()
+    parking_mask = models.MultiPolygonField(blank=True, null=True)
+    violator_mask = models.MultiPolygonField(blank=True, null=True)
     max_cap = models.PositiveIntegerField(default=0)
     current_cap = models.IntegerField(default=0)
-    organization = models.ForeignKey('users.Organization', blank=False, null=False, on_delete=models.PROTECT)
+    zone = models.CharField(choices=Zone.choices, max_length=244)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,22 +1,36 @@
+import uuid
+
 from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
 from django.contrib import messages
 
+from cameras.models import Camera
+
+
 class HomeView(View):
     template_name = "index.html"
 
-    def get(self, request,data=None, *args, **kwargs):
+    def get(self, request, data=None, *args, **kwargs):
         if request.user.is_authenticated:
             context = dict(user=request.user)
             if data == "cameras":
                 self.template_name = "cameras.html"
-
+                context['cameras'] = Camera.objects.all()
             return render(request, self.template_name, context=context)
         return render(request, 'login-form.html')
 
 
 class AddCameraView(View):
     def post(self, request):
-        print(request.POST)
-        messages.success(request, 'Camera added successfully.')
+        is_success = False
+        request_data = {k: v[0] for k, v in dict(request.POST).items() if k != 'csrfmiddlewaretoken'}
+        try:
+            uid = uuid.uuid4()
+            Camera.objects.create(**request_data, uid=uid)
+        except:
+            messages.error(request, 'Грешка при добавяне на камера')
+
+        if is_success:
+            messages.success(request, 'Камерата е добавена успешно')
+
         return HttpResponseRedirect('/cameras/')
